abf1af1dbd507c97745a07b1a0babba445ea2f17
Bytecoder <bytecoder0x@gmail.com>
2023-10-09 20:47:03 +0300

    test: hmac rfc 4231 vectors

--- hash/hmac.cpp
+++ hash/hmac.cpp
@@ -6,9 +6,33 @@
 
 using namespace std;
 
+bool failed = false;
+
+void check(const string& name, bool ok) {
+    cout << "check " << name << ": " << (ok ? "OK" : "FAIL") << endl;
+
+    if (!ok) {
+        failed = true;
+    }
+}
+
 int main() {
-    Bytes key = toBytes("Jefe");
-    Bytes data = toBytes("what do ya want for nothing?");
-    cout << "hmac-sha256(\"Jefe\", \"what do ya want for nothing?\") = " << toHex(hmacSha256(key, data)) << endl;
-    return 0;
+    string key = "Jefe";
+    string data = "what do ya want for nothing?";
+    cout << "hmac-sha256" << endl;
+    cout << "key: " << key << endl;
+    cout << "data: " << data << endl;
+    cout << "mac: " << toHex(hmacSha256(toBytes(key), toBytes(data))) << endl;
+
+    check("rfc case 1",
+          toHex(hmacSha256(Bytes(20, 0x0b), toBytes("Hi There"))) ==
+          "b0344c61d8db38535ca8afceaf0bf12b881dc200c9833da726e9376c2e32cff7");
+    check("rfc case 2",
+          toHex(hmacSha256(toBytes("Jefe"), toBytes("what do ya want for nothing?"))) ==
+          "5bdcc146bf60754e6a042426089575c75a003f089d2739839dec58b964ec3843");
+    check("rfc case 6, long key",
+          toHex(hmacSha256(Bytes(131, 0xaa), toBytes("Test Using Larger Than Block-Size Key - Hash Key First"))) ==
+          "60e431591ee0b67f0d8a26aacbf5b77f8e0bc6213728c5140546040f0ee37f54");
+
+    return failed ? 1 : 0;
 }
