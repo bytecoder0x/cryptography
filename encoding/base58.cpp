ced931916a52c2b9494724bd69054316b0f505a3
Bytecoder <bytecoder0x@gmail.com>
2023-11-15 19:08:37 +0200

    test: base58 bitcoin address vector

--- encoding/base58.cpp
+++ encoding/base58.cpp
@@ -1,20 +1,47 @@
 #include <iostream>
 
 #include "common/hex.h"
+#include "common/random.h"
 #include "encoding/base58.h"
 
 using namespace std;
 
+bool failed = false;
+
+void check(const string& name, bool ok) {
+    cout << "check " << name << ": " << (ok ? "OK" : "FAIL") << endl;
+
+    if (!ok) {
+        failed = true;
+    }
+}
+
 int main() {
-    Bytes input = toBytes("hello world");
-    string encoded = base58Encode(input);
+    cout << "base58 encoding" << endl;
+
+    Bytes helloBytes = toBytes("hello world");
+    string helloEncoded = base58Encode(helloBytes);
+    Bytes helloDecoded = base58Decode(helloEncoded);
 
     cout << "input: hello world" << endl;
-    cout << "encoded: " << encoded << endl;
-    cout << "decoded: " << toHex(base58Decode(encoded)) << endl;
+    cout << "encoded: " << helloEncoded << endl;
+    cout << "decoded: " << toHex(helloDecoded) << endl;
 
-    Bytes payload = fromHex("00010966776006953d5567439e5e39f86a0d273bee");
-    cout << "bitcoin address: " << base58Check(payload) << endl;
+    Bytes addressPayload = fromHex("00010966776006953d5567439e5e39f86a0d273bee");
+    string address = base58Check(addressPayload);
+    cout << "bitcoin payload: " << toHex(addressPayload) << endl;
+    cout << "bitcoin address: " << address << endl;
 
-    return 0;
+    Bytes solanaKey = bigIntToBytes(randomBits(256), 32);
+    string solanaEncoded = base58Encode(solanaKey);
+    Bytes solanaDecoded = base58Decode(solanaEncoded);
+
+    cout << "solana key: " << toHex(solanaKey) << endl;
+    cout << "solana key base58: " << solanaEncoded << endl;
+
+    check("hello world", helloEncoded == "StV1DL6CwTryKyV" && helloDecoded == helloBytes);
+    check("bitcoin address", address == "16UwLL9Risc3QfPqBUvKofHmBQ7wMtjvM");
+    check("solana key roundtrip", solanaDecoded == solanaKey);
+
+    return failed ? 1 : 0;
 }
